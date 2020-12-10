797c96c93c5599d01b1ed126ad03978f59578ab9
ned_g <nedgao@foxmail.com>
2020-12-10 18:09:43 +0800

    format code

--- app/chat/components/Avatar.tsx
+++ app/chat/components/Avatar.tsx
@@ -8,50 +8,54 @@ const failTimes = new Map();
  * @param e 事件
  */
 function handleError(e: SyntheticEvent) {
-    const times = failTimes.get(e.target) || 0;
-    if (times >= 2) {
-        return;
-    }
-    (e.target as HTMLImageElement).src = avatarFallback;
-    failTimes.set(e.target, times + 1);
+  const times = failTimes.get(e.target) || 0;
+  if (times >= 2) {
+    return;
+  }
+  (e.target as HTMLImageElement).src = avatarFallback;
+  failTimes.set(e.target, times + 1);
 }
 
 interface AvatarProps {
-    /** 头像链接 */
-    src: string;
-    /** 展示大小 */
-    size?: number;
-    /** 额外类名 */
-    className?: string;
-    /** 点击事件 */
-    onClick?: () => void;
-    onMouseEnter?: () => void;
-    onMouseLeave?: () => void;
+  /** 头像链接 */
+  src: string;
+  /** 展示大小 */
+  size?: number;
+  /** 额外类名 */
+  className?: string;
+  /** 点击事件 */
+  onClick?: () => void;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
 }
 
 const noop = () => {};
 
 function Avatar(props: AvatarProps) {
-    const {
-        src,
-        size = 60,
-        className = '',
-        onClick = noop,
-        onMouseEnter = noop,
-        onMouseLeave = noop,
-    } = props;
-    return (
-        <img
-            className={className}
-            style={{ width: size, height: size, borderRadius: size / 2 }}
-            src={/(blob|data):/.test(src) ? src : `${src}?imageView2/3/w/${size * 2}/h/${size * 2}`}
-            alt=""
-            onClick={onClick}
-            onError={handleError}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
-        />
-    );
+  const {
+    src,
+    size = 60,
+    className = '',
+    onClick = noop,
+    onMouseEnter = noop,
+    onMouseLeave = noop,
+  } = props;
+  return (
+    <img
+      className={className}
+      style={{ width: size, height: size, borderRadius: size / 2 }}
+      src={
+        /(blob|data):/.test(src)
+          ? src
+          : `${src}?imageView2/3/w/${size * 2}/h/${size * 2}`
+      }
+      alt=""
+      onClick={onClick}
+      onError={handleError}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    />
+  );
 }
 
 export default Avatar;
--- app/utils/uploadFile.ts
+++ app/utils/uploadFile.ts
@@ -1,20 +1,20 @@
 import fetch from './fetch';
 
 const loadQiniu = (() => {
-    let qiniu: any = null;
-    return async function handleLoadQiniu() {
-        if (qiniu) {
-            return qiniu;
-        }
+  let qiniu: any = null;
+  return async function handleLoadQiniu() {
+    if (qiniu) {
+      return qiniu;
+    }
 
-        // @ts-ignore
-        qiniu = await import(/* webpackChunkName: "qiniu" */ 'qiniu-js');
-        return qiniu;
-    };
+    // @ts-ignore
+    qiniu = await import(/* webpackChunkName: "qiniu" */ 'qiniu-js');
+    return qiniu;
+  };
 })();
 
 interface QiniuUploadInfo {
-    key: string;
+  key: string;
 }
 
 /**
@@ -25,54 +25,60 @@ interface QiniuUploadInfo {
  * @param qiniuNextEventCallback 七牛上传进度回调
  */
 export default async function uploadFile(
-    blob: Blob,
-    qiniuKey: string,
-    fileName: string,
-    qiniuNextEventCallback?: (info: QiniuUploadInfo) => void,
+  blob: Blob,
+  qiniuKey: string,
+  fileName: string,
+  qiniuNextEventCallback?: (info: QiniuUploadInfo) => void
 ): Promise<string> {
-    // 获取七牛上传token
-    const [getTokenErr, tokenRes] = await fetch('uploadToken');
-    if (getTokenErr) {
-        throw Error(getTokenErr);
-    }
+  // 获取七牛上传token
+  const [getTokenErr, tokenRes] = await fetch('uploadToken');
+  if (getTokenErr) {
+    throw Error(getTokenErr);
+  }
 
-    // 服务端返回标识, 说明七牛不可用, 则上传文件到服务端
-    if (tokenRes.useUploadFile === true) {
-        const [uploadErr, result] = await fetch('uploadFile', {
-            file: blob,
-            fileName,
-        });
-        if (uploadErr) {
-            throw Error(uploadErr);
-        }
-        if (qiniuNextEventCallback) {
-            qiniuNextEventCallback({
-                // @ts-ignore
-                total: {
-                    percent: 100,
-                },
-            });
-        }
-        return result.url;
-    }
-
-    // 七牛可用, 上传到七牛
-    const qiniu = await loadQiniu();
-    return new Promise((resolve, reject) => {
-        const result = qiniu.upload(blob, qiniuKey, tokenRes.token, { useCdnDomain: true }, {});
-        result.subscribe({
-            next: (info:QiniuUploadInfo) => {
-                if (qiniuNextEventCallback) {
-                    qiniuNextEventCallback(info);
-                }
-            },
-            error: (qiniuErr: any) => {
-                reject(qiniuErr);
-            },
-            complete: async (info: QiniuUploadInfo) => {
-                const imageUrl = `${tokenRes.urlPrefix + info.key}`;
-                resolve(imageUrl);
-            },
-        });
+  // 服务端返回标识, 说明七牛不可用, 则上传文件到服务端
+  if (tokenRes.useUploadFile === true) {
+    const [uploadErr, result] = await fetch('uploadFile', {
+      file: blob,
+      fileName,
     });
+    if (uploadErr) {
+      throw Error(uploadErr);
+    }
+    if (qiniuNextEventCallback) {
+      qiniuNextEventCallback({
+        // @ts-ignore
+        total: {
+          percent: 100,
+        },
+      });
+    }
+    return result.url;
+  }
+
+  // 七牛可用, 上传到七牛
+  const qiniu = await loadQiniu();
+  return new Promise((resolve, reject) => {
+    const result = qiniu.upload(
+      blob,
+      qiniuKey,
+      tokenRes.token,
+      { useCdnDomain: true },
+      {}
+    );
+    result.subscribe({
+      next: (info: QiniuUploadInfo) => {
+        if (qiniuNextEventCallback) {
+          qiniuNextEventCallback(info);
+        }
+      },
+      error: (qiniuErr: any) => {
+        reject(qiniuErr);
+      },
+      complete: async (info: QiniuUploadInfo) => {
+        const imageUrl = `${tokenRes.urlPrefix + info.key}`;
+        resolve(imageUrl);
+      },
+    });
+  });
 }
