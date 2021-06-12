8f6cf663aee0cc364090f19b4bd40b5801457a58
ned gao <642847505@qq.com>
2021-06-12 18:22:46 +0800

    修复刷新问题

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { history, configuredStore } from './store';
-import './app.global.css';
+import App from './App';
 
-import 'assets/css/material-dashboard-react.css?v=1.8.0';
-import './assets/css/normalize.less';
-
-const store = configuredStore();
-
-document.addEventListener('DOMContentLoaded', () => {
-  // eslint-disable-next-line global-require
-  const Root = require('./HomePage').default;
-  render(
-      <Root store={store} history={history} />,
-    document.getElementById('root')
-  );
-});
+render(<App />, document.getElementById('root'));
--- app/main.dev.ts
+++ app/main.dev.ts
@@ -6,7 +6,7 @@
  * through IPC.
  *
  * When running `yarn build` or `yarn build:main`, this file is compiled to
- * `./app/main.prod.js` using webpack. This gives us some performance wins.
+ * `./src/main.prod.js` using webpack. This gives us some performance wins.
  */
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
