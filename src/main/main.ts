745901f04152594054e187e3535e1033a44506db
nedphae <nedphae@gmail.com>
2022-06-15 00:25:29 +0800

    开发环境关闭electron安全，生产环境启用

--- src/main/main.ts
+++ src/main/main.ts
@@ -87,7 +87,7 @@ const createWindow = async () => {
     icon: getAssetPath('icon.png'),
     titleBarStyle: 'hidden',
     webPreferences: {
-      webSecurity: false,
+      webSecurity: !isDebug,
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
